975c4a8f91766fc558cf0254c1e5e746cb94e714
minhthong774 <minhthong774@gmail.com>
2022-06-14 22:58:13 +0700

    abc

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {library} from '@fortawesome/fontawesome-svg-core'
 import {faCheckCircle, faTimesCircle} from '@fortawesome/free-solid-svg-icons'
 
 library.add(faCheckCircle, faTimesCircle)
+const port = process.env.PORT || 3000;
 
 const container = document.getElementById('root');
 const root = createRoot(container);
